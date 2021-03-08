5292e11fcd941ff579aa0bdb8fa782cab43d92eb
Shade1996 <leizhiheng10@gmail.com>
2021-03-08 17:46:01 +0800

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import "tailwindcss/base.css"
+import "tailwindcss/components.css"
+import "tailwindcss/utilities.css"
+import "./App.css"
+
+import  React from 'react';
+import  ReactDOM from "react-dom";
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
